2d5ad5446caeb598ca3698f5d47b97985020ede3
jinyeanseok <wdtcwek11@naver.com>
2019-12-18 01:59:37 +0900

    google login 연동

--- src/main/java/com/was/webservice/springboot/web/IndexController.java
+++ src/main/java/com/was/webservice/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.was.webservice.springboot.web;
 
+import com.was.webservice.springboot.config.auth.dto.SessionUser;
 import com.was.webservice.springboot.service.posts.PostsService;
 import com.was.webservice.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -7,29 +8,35 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import javax.servlet.http.HttpSession;
+
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
-    private  final PostsService postsService;
-
+    private final PostsService postsService;
+    private final HttpSession httpSession;
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
     @GetMapping("/posts/save")
-    public String postsave() {
+    public String postsSave() {
         return "posts-save";
     }
-
 
     @GetMapping("/posts/update/{id}")
     public String postsUpdate(@PathVariable Long id, Model model) {
         PostsResponseDto dto = postsService.findById(id);
         model.addAttribute("post", dto);
+
         return "posts-update";
     }
 }
